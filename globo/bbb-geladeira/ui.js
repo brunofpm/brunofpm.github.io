6a8b0889e203d6baa438697505935c328424ba6b
Bruno Maranhão <Bruno Maranho>
2016-02-16 13:10:24 -0200

    geladeira bbb

---
+++ globo/bbb-geladeira/ui.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  
+	setTimeout(function() {
+		$(".area-1").addClass("animated bounceInDown");
+	}, 800);
+
+	setTimeout(function() {
+		$(".duplas").addClass("animated bounceInUp");
+	}, 1600);
+
+
+	/* Eliminada */
+
+	setTimeout(function() {
+		$(".dupla.um").addClass("eliminada animated shake");
+	}, 4000);
+
+});
